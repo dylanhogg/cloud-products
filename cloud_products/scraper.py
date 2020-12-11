90cabf20f41f3eff369f77f004c5b4ee41fee71f
dylan <dylanhogg@gmail.com>
2020-12-11 20:31:28 +1100

    Separate example run from full run.

--- cloud_products/scraper.py
+++ cloud_products/scraper.py
@@ -1,7 +1,19 @@
 import logging
 
 
-def run_aws_crawler():
+def run_full_aws_crawler():
+    logging.info("Run full AWS Crawler and save to files:")
+    from cloud_products.aws import AwsCrawler
+    crawler = AwsCrawler()
+
+    for product in AwsCrawler().get_products():
+        print(f"Saving {product.name}...")
+        crawler.save_product(product, output_path)
+
+    print(f"Finished.")
+
+
+def run_aws_crawler_examples():
     logging.info("Example 1: List AWS products")
     from cloud_products.aws import AwsCrawler
 
@@ -28,8 +40,21 @@ def run_aws_crawler():
     from cloud_products.aws import AwsCrawler
 
     for product in AwsCrawler().get_products()[0:5]:
-        print(f"Saving {product.name}")
+        print(f"Saving {product.name}...")
         crawler.save_product(product, output_path)
+
+    logging.info("Example 5: Convert list of products to Pandas dataframe")
+    import pandas as pd
+    from cloud_products.aws import AwsCrawler
+    products = AwsCrawler().get_products()
+    df = pd.DataFrame.from_records([vars(p) for p in products])
+    print(df)
+
+    logging.info("Example 6: Get Pandas dataframe of products:")
+    import pandas as pd
+    from cloud_products.aws import AwsCrawler
+    df = AwsCrawler().get_products_as_df(use_cache=True)
+    print(df)
 
 
 if __name__ == "__main__":
@@ -39,7 +64,8 @@ if __name__ == "__main__":
     output_path = "./_data/scrape_results/"
 
     if vendor == "aws":
-        run_aws_crawler()
+        run_aws_crawler_examples()
+        #run_full_aws_crawler()
 
     elif vendor == "gcp":
         raise Exception(f"Google Cloud Compute not currently supported")
