97c00708ab218da76f53c19b4bad027d9de098d0
Dylan Hogg <dylanhogg@gmail.com>
2023-11-03 14:26:30 +1100

    webdriver as a singleton.

--- cloud_products/cloud_products/browser.py
+++ cloud_products/cloud_products/browser.py
@@ -21,31 +21,30 @@ from selenium.webdriver.support import expected_conditions
 
 from selenium.webdriver.firefox.options import Options
 
+_options = Options()
+_options.add_argument("--headless")
+_driver = webdriver.Firefox(options=_options, service=Service(GeckoDriverManager().install()))
+
 
 class Browser(base.Crawler):
     def get_page_source(self, page: int) -> List[Product]:
         # TODO: add caching
         assert isinstance(page, int), "page must be an integer"
-
-        options = Options()
-        options.add_argument("--headless")
+        assert page > 0, "page must be greater than 0"
 
         url_format = ("https://aws.amazon.com/products/" 
                               "?aws-products-all.sort-by=item.additionalFields.productNameLowercase" 
                               "&aws-products-all.sort-order=asc&awsf.re%3AInvent=*all&awsf.Free%20Tier%20Type=*all&awsf.tech-category=*all" 
                               "&awsm.page-aws-products-all={page}")
         url_page = url_format.format(page=page)
-
-        # {"message":"API rate limit exceeded for 101.184.146.239. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)","documentation_url":"https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"}
-        driver = webdriver.Firefox(options=options, service=Service(GeckoDriverManager().install()))
-        driver.set_window_size(1120, 550)
-        driver.maximize_window()
-    
-        driver.get(url_page)
-        wait = WebDriverWait(driver, 40)
+        
+        _driver.set_window_size(1120, 550)
+        _driver.maximize_window()
+        _driver.get(url_page)
+        wait = WebDriverWait(_driver, 40)
         wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".m-card-main")))  # TODO: review class m-card-main
-        source = driver.page_source
-
-        driver.quit()  # TODO: reuse driver for other pages
-
+        source = _driver.page_source
         return source
+    
+    def quit(self):
+        _driver.quit()
